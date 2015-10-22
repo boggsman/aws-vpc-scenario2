86b0ce26476cbe93c586078ef832b3c6ffb8da5a
boggsman <zach@boggsman.com>
2015-10-22 11:26:46 +0000

    Fixed small issue with availability zone variable

--- subnets.tf
+++ subnets.tf
@@ -5,6 +5,7 @@ resource "aws_subnet" "public-subnet1" {
     vpc_id = "${aws_vpc.default.id}"
 
     cidr_block = "${var.public_subnet_cidr}"
+    availability_zone = "${var.aws_availability_zone}"
     tags {
         Name = "Public Subnet"
     }
@@ -18,6 +19,7 @@ resource "aws_subnet" "private-subnet1" {
     vpc_id = "${aws_vpc.default.id}"
     
     cidr_block = "${var.private_subnet_cidr}"
+    availability_zone = "${var.aws_availability_zone}"
     tags {
         Name = "Private Subnet"
     }
